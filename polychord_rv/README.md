3e508e9bcdac0aa228fca0b96f1ae247e6c3c920
Nicolas Unger <nicounger94@gmail.com>
2019-10-11 18:14:51 +0200

    Moved run.py to __init__ to save a space in the import

--- polychord_rv/README.md
+++ polychord_rv/README.md
@@ -1,1 +1,4 @@
-# run
+# PolyChord RV
+
+This is an implementarion to run radial velocity models with the Nested Sampling
+algorithm PolyChord.
---
+++ polychord_rv/__init__.py
@@ -0,0 +1,161 @@
+import os
+import sys
+import numpy as np
+import importlib
+import datetime
+import time
+from shutil import copy
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+import pypolychord as polychord
+import pypolychord.settings as polysettings
+
+# import PyPolyChord as polychord
+# import PyPolyChord.settings as polysettings
+
+from .config import read_config
+
+HOME = os.getenv('HOME')
+
+
+def runpoly(configfile, nlive=None, nplanets=None, modelargs={}, **kwargs):
+
+    # Read dictionaries from configuration file
+    rundict, datadict, priordict, fixeddict = read_config(configfile, nplanets)
+    parnames = list(priordict.keys())
+
+    # Import model module
+    models_path = os.path.join(os.getenv('HOME'), 'run/targets/{target}/models'.format(**rundict))
+    modulename = 'model_{target}_{runid}'.format(**rundict)
+    sys.path.insert(0, models_path)
+    mod = importlib.import_module(modulename) # modulename, models_path)
+
+    # Instantiate model class (pass additional arguments)
+    mymodel = mod.Model(fixeddict, datadict, priordict, **modelargs)
+
+    # Function to convert from hypercube to physical parameter space
+    def prior(hypercube):
+        """ Priors for each parameter. """
+        theta = []
+        for i, x in enumerate(hypercube):
+            theta.append(priordict[parnames[i]].ppf(x))
+        return theta    
+
+    # LogLikelihood
+    def loglike(x):
+        return (mymodel.lnlike(x), [])
+
+    # Prepare run
+    nderived = 0
+    ndim = len(parnames)
+
+    # Starting time to identify this specific run
+    # Define it only for rank 0 and broadcoast to the rest
+    if rank == 0:
+        isodate = datetime.datetime.today().isoformat()
+    else:
+        isodate = None
+    isodate = comm.bcast(isodate, root=0)
+
+    # Define PolyChord settings
+    settings = polysettings.PolyChordSettings(ndim, nderived, )
+    settings.do_clustering = True
+    if nlive is None:
+        settings.nlive = 25*ndim
+    else:
+        settings.nlive = nlive
+
+    # Define fileroot name (identifies this specific run)
+    fileroot = rundict['target']+'_'+rundict['runid']
+    if rundict['comment'] != '':
+        fileroot += '_'+rundict['comment']
+
+    # Label the run with nr of planets, live points, nr of cores, sampler and date
+    fileroot += '_k{}'.format(mymodel.nplanets)
+    fileroot += '_nlive{}'.format(settings.nlive)
+    fileroot += '_ncores{}'.format(size)
+    fileroot += '_polychord'
+    fileroot += '_'+isodate
+
+    settings.file_root = fileroot
+    settings.read_resume = False
+    settings.num_repeats = ndim * 5
+    settings.feedback = 1
+    settings.precision_criterion = 0.01
+    # Base directory
+    if 'spectro' in HOME:
+        # If it's runing in cluster -> save in scratch folder
+        base_dir = os.path.join('/scratch/nunger', 'ExP', rundict['target'], fileroot, 'polychains')
+    else:
+        # Running locally
+        base_dir = os.path.join(HOME, 'ExP', rundict['target'], fileroot, 'polychains')
+    settings.base_dir = base_dir
+
+    # Initialise clocks
+    ti = time.clock()
+    tw = time.time()
+
+    # Run PolyChord
+    output = polychord.run_polychord(loglike, ndim, nderived, settings, prior)
+
+    # Assign additional parameters to output
+    paramnames = [(x, x) for x in parnames]
+    output.make_paramnames_files(paramnames)
+    output.runtime = time.clock() - ti
+    output.walltime = time.time() - tw
+    output.target = rundict['target']
+    output.runid = rundict['runid']
+    output.comment = rundict.get('comment', '')
+    output.nplanets = mymodel.nplanets
+    output.nlive = settings.nlive
+    output.isodate = isodate
+    output.ncores = size
+
+    if output.comment != '':
+        output.comment = '_'+output.comment
+
+    run_time = datetime.timedelta(seconds=int(output.runtime))
+    wall_time = datetime.timedelta(seconds=int(output.walltime))
+
+    if rank == 0:
+        # Print run time
+        print('\nTotal run time was: {}'.format(run_time))
+        print('Total wall time was: {}'.format(wall_time))
+
+        # Save output as pickle file
+        dump2pickle_poly(output)
+
+        # Copy post processing script to this run's folder
+        copy(os.path.join(HOME,'run/post_processing.py'), os.path.join(output.base_dir, '..'))
+
+    return output
+
+
+def dump2pickle_poly(output, savedir=None):
+    """ Takes the output from PolyChord and saves it as a pickle file. """
+
+    try:
+        import pickle
+    except ImportError:
+        print('Install pickle to save the output. Try running:\n pip install pickle')
+        return
+
+    if savedir is None:
+        # Save directory is parent of base dir
+        pickledir = os.path.join(output.base_dir, '..')
+    else:
+        # Unless specified otherwhise
+        pickledir = savedir
+
+    # Check if path exists; create if not
+    if not os.path.isdir(pickledir):
+        os.makedirs(pickledir)
+
+    f = open(os.path.join(pickledir, output.file_root+'.dat'), 'wb')
+
+    pickle.dump(output, f)
+    f.close()
+    return
